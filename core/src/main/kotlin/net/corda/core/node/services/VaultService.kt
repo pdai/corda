7654c952eb49cba4580724ebcc2276b23bc7ff89
Konstantinos Chalkias <konstantinos.chalkias@r3.com>
2018-02-23 17:37:12 +0000

    VaultService update imports and comment/references issues

--- core/src/main/kotlin/net/corda/core/node/services/VaultService.kt
+++ core/src/main/kotlin/net/corda/core/node/services/VaultService.kt
@@ -6,13 +6,14 @@ import net.corda.core.concurrent.CordaFuture
 import net.corda.core.contracts.*
 import net.corda.core.crypto.SecureHash
 import net.corda.core.flows.FlowException
+import net.corda.core.flows.FlowLogic
 import net.corda.core.identity.AbstractParty
 import net.corda.core.messaging.DataFeed
-import net.corda.core.node.services.vault.PageSpecification
-import net.corda.core.node.services.vault.QueryCriteria
-import net.corda.core.node.services.vault.Sort
+import net.corda.core.node.services.Vault.StateStatus
+import net.corda.core.node.services.vault.*
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.toFuture
+import net.corda.core.transactions.LedgerTransaction
 import net.corda.core.utilities.NonEmptySet
 import rx.Observable
 import java.time.Instant
@@ -115,15 +116,15 @@ class Vault<out T : ContractState>(val states: Iterable<StateAndRef<T>>) {
     /**
      * Returned in queries [VaultService.queryBy] and [VaultService.trackBy].
      * A Page contains:
-     *  1) a [List] of actual [StateAndRef] requested by the specified [QueryCriteria] to a maximum of [MAX_PAGE_SIZE]
-     *  2) a [List] of associated [Vault.StateMetadata], one per [StateAndRef] result
-     *  3) a total number of states that met the given [QueryCriteria] if a [PageSpecification] was provided
-     *     (otherwise defaults to -1)
-     *  4) Status types used in this query: UNCONSUMED, CONSUMED, ALL
-     *  5) Other results as a [List] of any type (eg. aggregate function results with/without group by)
+     *  1) a [List] of actual [StateAndRef] requested by the specified [QueryCriteria] to a maximum of [MAX_PAGE_SIZE].
+     *  2) a [List] of associated [Vault.StateMetadata], one per [StateAndRef] result.
+     *  3) a total number of states that met the given [QueryCriteria] if a [PageSpecification] was provided,
+     *     otherwise it defaults to -1.
+     *  4) Status types used in this query: [StateStatus.UNCONSUMED], [StateStatus.CONSUMED], [StateStatus.ALL].
+     *  5) Other results as a [List] of any type (eg. aggregate function results with/without group by).
      *
      *  Note: currently otherResults are used only for Aggregate Functions (in which case, the states and statesMetadata
-     *  results will be empty)
+     *  results will be empty).
      */
     @CordaSerializable
     data class Page<out T : ContractState>(val states: List<StateAndRef<T>>,
@@ -198,7 +199,7 @@ interface VaultService {
 
     /**
      * Reserve a set of [StateRef] for a given [UUID] unique identifier.
-     * Typically, the unique identifier will refer to a [FlowLogic.runId.uuid] associated with an in-flight flow.
+     * Typically, the unique identifier will refer to a [FlowLogic.runId]'s [UUID] associated with an in-flight flow.
      * In this case if the flow terminates the locks will automatically be freed, even if there is an error.
      * However, the user can specify their own [UUID] and manage this manually, possibly across the lifetime of multiple flows,
      * or from other thread contexts e.g. [CordaService] instances.
@@ -223,7 +224,7 @@ interface VaultService {
      * Helper function to determine spendable states and soft locking them.
      * Currently performance will be worse than for the hand optimised version in `Cash.unconsumedCashStatesForSpending`
      * However, this is fully generic and can operate with custom [FungibleAsset] states.
-     * @param lockId The [FlowLogic.runId.uuid] of the current flow used to soft lock the states.
+     * @param lockId The [FlowLogic.runId]'s [UUID] of the current flow used to soft lock the states.
      * @param eligibleStatesQuery A custom query object that selects down to the appropriate subset of all states of the
      * [contractStateType]. e.g. by selecting on account, issuer, etc. The query is internally augmented with the UNCONSUMED,
      * soft lock and contract type requirements.
@@ -247,12 +248,12 @@ interface VaultService {
      * and returns a [Vault.Page] object containing the following:
      *  1. states as a List of <StateAndRef> (page number and size defined by [PageSpecification])
      *  2. states metadata as a List of [Vault.StateMetadata] held in the Vault States table.
-     *  3. total number of results available if [PageSpecification] supplied (otherwise returns -1)
-     *  4. status types used in this query: UNCONSUMED, CONSUMED, ALL
-     *  5. other results (aggregate functions with/without using value groups)
+     *  3. total number of results available if [PageSpecification] supplied (otherwise returns -1).
+     *  4. status types used in this query: [StateStatus.UNCONSUMED], [StateStatus.CONSUMED], [StateStatus.ALL].
+     *  5. other results (aggregate functions with/without using value groups).
      *
      * @throws VaultQueryException if the query cannot be executed for any reason
-     *        (missing criteria or parsing error, paging errors, unsupported query, underlying database error)
+     *        (missing criteria or parsing error, paging errors, unsupported query, underlying database error).
      *
      * Notes
      *   If no [PageSpecification] is provided, a maximum of [DEFAULT_PAGE_SIZE] results will be returned.
@@ -269,11 +270,11 @@ interface VaultService {
     /**
      * Generic vault query function which takes a [QueryCriteria] object to define filters,
      * optional [PageSpecification] and optional [Sort] modification criteria (default unsorted),
-     * and returns a [Vault.PageAndUpdates] object containing
-     * 1) a snapshot as a [Vault.Page] (described previously in [queryBy])
-     * 2) an [Observable] of [Vault.Update]
+     * and returns a [DataFeed] object containing
+     * 1) a snapshot as a [Vault.Page] (described previously in [queryBy]).
+     * 2) an [Observable] of [Vault.Update].
      *
-     * @throws VaultQueryException if the query cannot be executed for any reason
+     * @throws VaultQueryException if the query cannot be executed for any reason.
      *
      * Notes: the snapshot part of the query adheres to the same behaviour as the [queryBy] function.
      *        the [QueryCriteria] applies to both snapshot and deltas (streaming updates).
